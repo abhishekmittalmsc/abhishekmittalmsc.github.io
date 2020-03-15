394435c307badc3695664ffdb543e629ac965b92
abhishekmittalmsc <59051286+abhishekmittalmsc@users.noreply.github.com>
2020-03-15 22:04:21 +0530

    Starwars
    Starwars

---
+++ precache-manifest.9baed50a7230ef72777c2692edd88ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3cbd1bcf38c416a3f506bdef76cd18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d17ed30a9096e4e78d4",
+    "url": "/static/css/main.22fb832e.chunk.css"
+  },
+  {
+    "revision": "3e7b7ed127a67e40a5d1",
+    "url": "/static/js/2.dcfd2871.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/static/js/2.dcfd2871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d17ed30a9096e4e78d4",
+    "url": "/static/js/main.fd0aeea6.chunk.js"
+  },
+  {
+    "revision": "0965cc6b4195852a0b1c",
+    "url": "/static/js/runtime-main.aa3f42f6.js"
+  }
+]);
